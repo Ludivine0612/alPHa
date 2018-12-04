107d31b039ada786c601191ebc7fd419cd7ce228
Ludivine Sevilla <lu0612@hotmail.com>
2018-12-04 17:53:35 +0100

    bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
-  # belongs_to :planning
-  # belongs_to :client
-  # belongs_to :employee
-  # belongs_to :service
+  belongs_to :planning
+  belongs_to :client
+  #belongs_to :employee
+  belongs_to :service
 
   validates :start_date, presence: true
   validates :end_date, presence: true
