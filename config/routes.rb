a5caa187b21428762ed05731d09b278d333b1c1e
jonathan <jonathanremymichel@laposte.net>
2018-12-06 15:42:06 +0100

    banner ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#banner'
+  get '/home', to: 'pages#home'
 
   devise_for :user
-  resources :booking, only: :index, :show
+  resources :booking, only: [:index, :show]
   resources :prestation, only: :update
 end
