a0d4ec90b700afa1b2f7e80a14518a59074b2ba2
Julien Vallar <julien.vallar@gmail.com>
2018-12-07 14:54:07 +0100

    geocoding

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   belongs_to :company
   belongs_to :client, class_name: 'User', foreign_key: :user_id
   has_many :prestations
