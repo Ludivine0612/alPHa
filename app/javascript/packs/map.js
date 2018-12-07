329c4b982c4f6b1d216c421131e793dcb9bc255e
Julien Vallar <julien.vallar@gmail.com>
2018-12-07 15:22:19 +0100

    add change

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -31,7 +31,7 @@ if (mapElement) { // only build a map if there's a div#map to inject into
     markers.forEach((marker) => {
       bounds.extend([marker.lng, marker.lat]);
     });
-    map.fitBounds(bounds, { duration: 5000, padding: 75 })
+    map.fitBounds(bounds, { duration: 10000, padding: 75 })
   }
   map.addControl(new MapboxGeocoder({
     accessToken: mapboxgl.accessToken
