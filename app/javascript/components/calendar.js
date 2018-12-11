f585bfe3072fe7fca5d2be0e3cd23ab695dec71a
Damien Delahaye <damien.delahaye2@gmail.com>
2018-12-11 12:23:36 +0100

    saloperie de JS

--- app/javascript/components/calendar.js
+++ app/javascript/components/calendar.js
@@ -32,7 +32,21 @@ $('#calendar').fullCalendar({
       right:  'today prev,next'
     },
     hiddenDays: [ 0 ],  // ici je cache le dimanche
-    events: @events
+    events:[
+    {
+      title  : 'event1',
+      start  : '2010-01-01'
+    },
+    {
+      title  : 'event2',
+      start  : '2010-01-05',
+      end    : '2010-01-07'
+    },
+    {
+      title  : 'event3',
+      start  : '2010-01-09T12:30:00'
+    }
+  ]
 });
 
 
